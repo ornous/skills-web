5d05262fca049684547e9c73e92b408dbbbb37d0
Ousmane Ndiaye <snekshaark@gmail.com>
2017-10-13 19:28:22 +0100

    Add basic CSS Grid

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import logo from './logo.svg'
 
 import SkillsList from './containers/SkillsList'
 
-const Wrapper = styled.div`text-align: center;`
+const Wrapper = styled.div`
+  text-align: center;
+  max-width: 940px;
+  margin: 0 auto;
+  display: grid;
+  grid-template: 'head head' 'nav  main' 'nav  foot';
+  grid-template-columns: 180px 1fr;
+  grid-template-rows: 150px auto 150px;
+  grid-gap: 10px;
+  height: 100vh;
+  width: 100vw;
+`
+
+const LeftSideBar = styled.div`
+  background-color: palevioletred;
+  grid-area: nav;
+  padding: 10px;
+`
+
+const Main = styled.div`
+  background-color: lightblue;
+  padding: 10px;
+  grid-area: main;
+`
 
 const Logo = styled.img`height: 80px;`
 
@@ -20,20 +43,22 @@ const RotatingLogo = Logo.extend`animation: ${rotate360} infinite 20s linear;`
 
 const Header = styled.div`
   background-color: #222;
-  height: 150px;
   padding: 20px;
   color: white;
+  grid-area: head;
+  line-height: 100%;
+  vertical-align: middle;
+`
+
+const Footer = styled.div`
+  grid-area: foot;
+  padding: 10px;
+  vertical-align: middle;
+  background-color: tomato;
 `
 
 const Intro = styled.p`font-size: large;`
 
-/**
- * Inject features:
- * - react-router
- * - add page abstraction
- * - add helmet
- * - switch to styled-components
- */
 class App extends Component {
   render () {
     return (
@@ -41,8 +66,18 @@ class App extends Component {
         <Header>
           <RotatingLogo src={logo} className='App-logo' alt='logo' />
         </Header>
-        <Intro>A very thoughtful intro lies here</Intro>
-        Ozzy's Skills: <SkillsList />
+        <LeftSideBar>To the left (bis)</LeftSideBar>
+        <Main>
+          <Intro>A very thoughtful intro lies here</Intro>
+          Ozzy's Skills: <SkillsList />
+        </Main>
+        <Footer>
+          <p>
+            I am usually at the bottom of the page linking to things no one
+            cares about until they do. I am an unsung here that most hate In
+            football, I am more common than a header What am I?
+          </p>
+        </Footer>
       </Wrapper>
     )
   }
